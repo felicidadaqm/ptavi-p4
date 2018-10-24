822bc84c4144c712271d94ca527cd4aa57915e9e
Felicidad Abad Quintanilla <fabadq@beta09.aulas.gsyc.urjc.es>
2018-10-24 10:34:16 +0200

    EJ5 acabado

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -20,11 +21,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
+            print(self.client_address[0])
+            print(self.client_address[1])
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    port = int(sys.argv[1])
+    serv = socketserver.UDPServer(('', port), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
